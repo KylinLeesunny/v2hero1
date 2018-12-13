2d80a8861bb2152bb178e869b9993f3560285d66
ThisisVip <374389391@qq.com>
2018-12-13 23:24:19 +0800

    test1

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+cd /v2ray
+chmod +x v2ray
+chmod +x v2ctl
+
+#echo -e -n "$CONFIG_JSON1" > config.json
+#echo -e -n "$PORT" >> config.json
+#echo -e -n "$CONFIG_JSON2" >> config.json
+#echo -e -n "$UUID" >> config.json
+#echo -e -n "$CONFIG_JSON3" >> config.json
+
+
+./v2ray
